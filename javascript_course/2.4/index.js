3bea6fdc2d2ac72509b702a8c1522a91cc25ef17
Ori Sailes <77921279+orisailes@users.noreply.github.com>
2021-02-07 17:57:03 +0200

    Add files via upload

---
+++ javascript_course/2.4/index.js
@@ -0,0 +1,19 @@
+var x = new Date();
+var monthNumber = x.getUTCDate()
+var dayNumber = x.getDay()
+var date = x.getUTCDate()
+var month = x.getMonth()
+var year = x.getUTCFullYear();
+
+// defining day
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+// defining month
+var months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July' , 'ETC'];
+
+
+
+
+console.log(`The day is ${days[dayNumber]}`)
+console.log(`The day in month is ${date}`)
+console.log(`The month is ${months[month + 1]}`)
+console.log(`The year is ${year}`)
