6761c180c905de8bdec687d78e435f63d1a39bf7
Ori Sailes <77921279+orisailes@users.noreply.github.com>
2021-02-16 11:46:54 +0200

    Add files via upload

---
+++ javascript_course/13.1/index.js
@@ -0,0 +1,11 @@
+const listOfNeighbours = [
+    ["Canada", "Mexico"],
+    ["Spain"],
+    ["Norway", "Sweden", "Russia"],
+];
+
+for(let i of listOfNeighbours){
+    for(let x of i){
+        console.log(x)
+    }
+}
