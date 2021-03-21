53d37953757a0d3e2cebf2a5a08d86bdaaf602ab
Ori Sailes <77921279+orisailes@users.noreply.github.com>
2021-03-21 22:31:11 +0200

    Add files via upload

--- react/src/App.js
+++ react/src/App.js
@@ -4,18 +4,27 @@ import React from 'react'
 class app extends React.Component {
     constructor(props) {
         super(props);
-        this.myInput = React.createRef()
+        this.myTextArea = React.createRef()
+        this.state={
+            text:`copy me`
+        }
     }
-    componentDidMount = () => {
-        this.myInput.focus()
-    }
-    
 
+    handleChange = (e) => {
+        this.setState({text:e.target.value});
+    }
+
+    makeCopy = () =>{
+        const textToCopy = this.myTextArea.current.value;
+        this.myTextArea.current.select();
+        document.execCommand('copy')
+    }
 
     render() {
         return (
             <div>
-                <input type="text" ref={ref => this.myInput = ref} />
+                <textarea onChange={this.handleChange} value={this.state.text} ref={this.myTextArea} cols="20" rows="4"></textarea>
+                <button onClick={this.makeCopy}>Copy</button>
             </div>
         )
     }
